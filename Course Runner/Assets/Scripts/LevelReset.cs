e765fc1da554fac95ab7392b4cb43795151cd93a
diggorylord <diggorylord@gmail.com>
2017-03-06 13:57:46 +0000

    moved the end level pickup more to the center so its easier to spot rather than to the left.

---
+++ Course Runner/Assets/Scripts/LevelReset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			SceneManager.LoadScene ("Level 1");
+		}
+	}
+}
