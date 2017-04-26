fd24f58decf57b08b1d4a9f1b3485a0811a80b8e
diggorylord <diggorylord@gmail.com>
2017-04-26 13:13:46 +0100

    Ran into some problems and then had trouble reverting so had to delete some files to recover an older version. so levels 1 and 2 are there with the main menu.

--- Course Runner/Assets/Scripts/LevelReset.cs
+++ Course Runner/Assets/Scripts/LevelReset.cs
@@ -4,12 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class LevelReset : MonoBehaviour
 {
+	public string sceneToLoad;
+
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			string currentScene = SceneManager.GetActiveScene ().name;
-			SceneManager.LoadScene (currentScene);
+			SceneManager.LoadScene (sceneToLoad);
 		}
 	}
 }
